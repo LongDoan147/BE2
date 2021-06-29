3114601336e7553ef1122735241b4838ba97c6df
Long Doan <doanho147@gmail.com>
2021-06-29 13:45:51 +0700

    new commit

--- blog/resources/views/login.blade.php
+++
@@ -1,7 +0,0 @@
-<form action="{{ url('/admin') }}" method="get">
-        <label for="">User:</label>
-        <input type="text" name="user"><br>
-        <label for="">Password:</label>
-        <input type="text" name="pass"><br>
-        <input type="submit">
-    </form>
